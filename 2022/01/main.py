afb20b24c3628a9b9c716192eb73082432d5cfe4
Mitch Morrison <rjmitmor@gmail.com>
2022-12-02 11:22:42 -0400

    merge from local

---
+++ 2022/01/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+inventory = []
+
+def get_totals(inv):
+    summed = []
+    for l in inv:
+        total = 0
+        for i in l:
+            total = total + int(i)
+        summed.append(total)
+    return(summed)
+
+
+def get_biggest(totals):
+    top = []
+    all_items = totals
+    sums = 0
+    while len(top) < 3:
+        highest = 0
+        for i in all_items:
+            if i > highest:
+                highest = i
+        top.append(highest)
+        all_items.remove(highest)
+    for i in top:
+        sums = sums + i
+    return sums
+
+with open('input.txt','r') as f:
+    c = []
+    for i in f.readlines():
+        if i == '\n':
+            inventory.append(c)
+            c = []
+        else:
+            c.append(i.strip())
+
+print(get_biggest(get_totals(inventory)))
+
+            
+
+
